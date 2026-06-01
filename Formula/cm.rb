1a0ba57ba2baca0615446cecc19c00a6a5a2fc65
wenqiangde <andmaster@163.com>
2026-06-01 17:27:27 +0800

    chore: add homebrew formula

---
+++ Formula/cm.rb
@@ -0,0 +1,17 @@
+class Cm < Formula
+  desc "SSH, VNC, and rsync profile manager"
+  homepage "https://github.com/zhangwenqiang-dev/homebrew-tools"
+  url "git@github.com-work2:zhangwenqiang-dev/homebrew-tools.git",
+      tag: "v0.1.1"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"cm", "./cmd/cm"
+  end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/cm --help")
+  end
+end
