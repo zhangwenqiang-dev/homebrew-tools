bd3dce6420881bae2c00209c42cc4d9c13a7d3fe
wenqiangde <andmaster@163.com>
2026-06-03 14:48:48 +0800

    Release cm v0.1.7

--- Formula/cm.rb
+++ Formula/cm.rb
@@ -2,7 +2,7 @@ class Cm < Formula
   desc "SSH, VNC, and rsync profile manager"
   homepage "https://github.com/zhangwenqiang-dev/homebrew-tools"
   url "https://github.com/zhangwenqiang-dev/homebrew-tools.git",
-      tag: "v0.1.6"
+      tag: "v0.1.7"
   license "MIT"
 
   depends_on "go" => :build
