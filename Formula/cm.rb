42fdc618652ba724b1d40e03b1d3fb9b9319aacb
wenqiangde <andmaster@163.com>
2026-07-07 18:38:26 +0800

    Remove header status badge

--- Formula/cm.rb
+++ Formula/cm.rb
@@ -2,7 +2,7 @@ class Cm < Formula
   desc "SSH, VNC, and rsync profile manager"
   homepage "https://github.com/zhangwenqiang-dev/homebrew-tools"
   url "https://github.com/zhangwenqiang-dev/homebrew-tools.git",
-      tag: "v0.1.82"
+      tag: "v0.1.83"
   license "MIT"
 
   depends_on "go" => :build
