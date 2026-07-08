e265f8a56df5ee8a28ac97a1fa24bc2fd7d08007
wenqiangde <andmaster@163.com>
2026-07-08 10:39:16 +0800

    Use modal for adding members

--- Formula/cm.rb
+++ Formula/cm.rb
@@ -2,7 +2,7 @@ class Cm < Formula
   desc "SSH, VNC, and rsync profile manager"
   homepage "https://github.com/zhangwenqiang-dev/homebrew-tools"
   url "https://github.com/zhangwenqiang-dev/homebrew-tools.git",
-      tag: "v0.1.88"
+      tag: "v0.1.89"
   license "MIT"
 
   depends_on "go" => :build
